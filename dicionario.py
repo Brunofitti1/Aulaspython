feaff57cfdd34e8c0776991596c6378cefa30f2b
Brunofitti1 <brunofitti1@gmail.com>
2023-08-07 22:46:41 -0300

    aula sobre dicionario.py

--- dicionario.py
+++ dicionario.py
@@ -1,1 +1,25 @@
 #dicionario
+"""tabela = {"alface": 0.45, "batata": 1.2 , "tomate": 2.3 , "feijão": 1.5}
+while True:
+    produto = input("digite o nome do produto ou  fim para terminar")
+    if produto = "fim":
+        break
+    if produto in tabela:
+        print(f"preço: R$ {tabela[produto]:.2f}")
+    else:
+        print("produto nao encontrado.")"""
+
+
+carros = {"Jimmy": 75000.00, "Toro": 120000.00, "Amarok": 180000.00, "Audi A3": 215000.00, "Urus": 1200000.00,
+          "Maverick": 200000.00,}
+print(carros.keys())
+carros = {"Jimmy": 75000.00, "Toro": 120000.00, "Amarok": 180000.00, "Audi A3": 215000.00, "Urus": 1200000.00,
+          "Maverick": 200000.00,}
+while True:
+    carro = input("digite o nome do carro de sua escolha ou difite fim para terimnar.")
+    if carro == "fim":
+        break
+    if carro in carros:
+        print(f"Preço: R${carros[carro]:.2f}")
+    else:
+        print("carro nâo encontrado")
